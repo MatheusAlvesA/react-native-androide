96efa106e57547ecc157d4cf6099a7baaf2a4682
Matheus Alves de Andrade <matheuseejam@gmail.com>
2019-05-10 22:54:01 -0300

    Implemented first functions

--- android/src/main/java/com/rnandroid/SoundModule.java
+++ android/src/main/java/com/rnandroid/SoundModule.java
@@ -1,5 +1,12 @@
 package com.rnandroid;
 
+import java.io.File;
+import java.io.IOException;
+
+import android.net.Uri;
+import android.media.MediaPlayer;
+import android.content.res.AssetFileDescriptor;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -9,14 +16,97 @@ import com.facebook.react.bridge.Promise;
 
 public class SoundModule extends ReactContextBaseJavaModule {
 
+  MediaPlayer mediaPlayer;
+  ReactApplicationContext context;
+
   public SoundModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    // TODO
+    this.context = reactContext;
   }
 
   @Override
   public String getName() {
     return "Sound";
+  }
+
+  @ReactMethod
+  public void prepare(String source, Promise p) {
+    try {
+      this.prepareMediaPlayer(source);
+      p.resolve(true);
+    } catch (Exception e) {
+      p.reject(e);
+    }
+  }
+
+  @ReactMethod
+  public void play(Promise p) {
+    if(this.mediaPlayer == null) {
+      p.reject("ERROR", "Media Player is not prepareted");
+      return;
+    }
+
+    try {
+      this.mediaPlayer.start();
+      p.resolve(true);
+    } catch (IllegalStateException e) {
+      p.reject(e);
+    }
+  }
+
+  private void prepareMediaPlayer(String fileName) throws IOException {
+    fileName = this.removeFileExtension(fileName);
+    if(this.mediaPlayer != null)
+      this.mediaPlayer.release();
+
+    int res = this.context.getResources()
+                          .getIdentifier(
+                                          fileName,
+                                          "raw",
+                                          this.context.getPackageName()
+                                        );
+    if(res != 0) { // Is a resource
+      this.mediaPlayer = new MediaPlayer();
+      try (AssetFileDescriptor afd = context.getResources().openRawResourceFd(res)) {
+        this.mediaPlayer.setDataSource(
+                                  afd.getFileDescriptor(),
+                                  afd.getStartOffset(),
+                                  afd.getLength()
+                                );
+        this.mediaPlayer.prepare();
+      }
+      return;
+    }
+
+    // It's from the Internet
+    if(fileName.startsWith("http://") || fileName.startsWith("https://")) {
+      Uri uri = Uri.parse(fileName);
+      this.mediaPlayer = MediaPlayer.create(this.context, uri);
+      if(this.mediaPlayer == null)
+        throw new IOException("Failed to open uri: ".concat(fileName));
+      this.mediaPlayer.prepare();
+    }
+
+    File file = new File(fileName);
+    if (file.exists()) { // It's from the file system
+      this.mediaPlayer.setDataSource(fileName);
+      this.mediaPlayer.prepare();
+      return;
+    }
+
+    // Is undetermined
+    throw new IOException("Failed to open uri: ".concat(fileName));
+  }
+
+  private String removeFileExtension(String source) {
+    String[] splited = source.split("[.]");
+    StringBuilder joined = new StringBuilder();
+
+    for(int i = 0; i < splited.length-1; i++) {
+      joined.append(splited[i]);
+    }
+
+    return joined.toString();
   }
 
 }
